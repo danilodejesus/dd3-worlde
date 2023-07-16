fe0ee42103fdc1718f46056dfe558a2c1ebf8c00
Danilo Viacava <danilojesusv@gmail.com>
2023-07-16 04:28:46 -0500

    add typescript

--- src/environments/environment.local.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  dictionaryWords: 'https://gitlab.com/d2945/words/-/raw/main/words.txt',
-  gSuiteclientId: '228273372061-j1m6j8mhlk9cb99tgcblesvucg0vohul.apps.googleusercontent.com',
-};
