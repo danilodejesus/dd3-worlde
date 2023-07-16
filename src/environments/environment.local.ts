c23a1a01818d4acdc6e4d4e49d4f2786dd4d0565
Danilo Viacava <danilojesusv@gmail.com>
2023-07-16 04:19:29 -0500

    setup react

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  dictionaryWords: 'https://gitlab.com/d2945/words/-/raw/main/words.txt',
+  gSuiteclientId: '228273372061-j1m6j8mhlk9cb99tgcblesvucg0vohul.apps.googleusercontent.com',
+};
